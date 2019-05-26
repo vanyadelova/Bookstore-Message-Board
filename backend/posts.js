98a928e561faccc957268297b3c99a7fcff34ed0
vanyadelova <happy.vanya.delova@gmail.com>
2019-05-26 10:41:59 +0200

    working on default posts

---
+++ backend/posts.js
@@ -0,0 +1,151 @@
+const clone = require('clone')
+
+let db = {}
+
+const defaultData = {
+  "8xf0y6ziyjabvozdd253nd": {
+    id: '8xf0y6ziyjabvozdd253nd',
+    timestamp: 1368479767190,
+    title: 'The Tree Frog',
+    body: "Summer is never boring when you\'ve got a book.",
+    author: 'foobear',
+    category: 'books',
+    voteScore: 5,
+    deleted: false,
+    commentCount: 0
+  },
+  "9xf0y6ziyjabvozdd253nd": {
+    id: '9xf0y6ziyjabvozdd253nd',
+    timestamp: 1519269415880,
+    title: 'Night Frogs',
+    body: 'Storytel\'s audio books are the best!',
+    author: 'frog aficionado',
+    category: 'books',
+    voteScore: 2,
+    deleted: false,
+    commentCount: 0
+  },
+  "6ni6ok3ym7mf1p33lnez": {
+    id: '6ni6ok3ym7mf1p33lnez',
+    timestamp: 1467166872634,
+    title: 'Cat',
+    body: 'I see a book, I see a coffee, I see a good day ahead.',
+    author: 'thingone',
+    category: 'audio-books',
+    voteScore: -4,
+    deleted: false,
+    commentCount: 0
+  }
+}
+
+function getData (token) {
+  let data = db[token]
+  if (data == null) {
+    data = db[token] = clone(defaultData)
+  }
+  return data
+}
+
+function getByCategory (token, category) {
+  return new Promise((res) => {
+    let posts = getData(token)
+    let keys = Object.keys(posts)
+    let filtered_keys = keys.filter(key => posts[key].category === category && !posts[key].deleted)
+    res(filtered_keys.map(key => posts[key]))
+  })
+}
+
+function get (token, id) {
+  return new Promise((res) => {
+    const posts = getData(token)
+    res(
+      posts[id].deleted
+        ? {}
+        : posts[id]
+    )
+  })
+}
+
+function getAll (token) {
+  return new Promise((res) => {
+    const posts = getData(token)
+    let keys = Object.keys(posts)
+    let filtered_keys = keys.filter(key => !posts[key].deleted)
+    res(filtered_keys.map(key => posts[key]))
+  })
+}
+
+function add (token, post) {
+  return new Promise((res) => {
+    let posts = getData(token)
+
+    posts[post.id] = {
+      id: post.id,
+      timestamp: post.timestamp,
+      title: post.title,
+      body: post.body,
+      author: post.author,
+      category: post.category,
+      voteScore: 1,
+      deleted: false,
+      commentCount: 0
+    }
+
+    res(posts[post.id])
+  })
+}
+
+function vote (token, id, option) {
+  return new Promise((res) => {
+    let posts = getData(token)
+    post = posts[id]
+    switch(option) {
+        case "upVote":
+            post.voteScore = post.voteScore + 1
+            break
+        case "downVote":
+            post.voteScore = post.voteScore - 1
+            break
+        default:
+            console.log(`posts.vote received incorrect parameter: ${option}`)
+    }
+    res(post)
+  })
+}
+
+function disable (token, id) {
+    return new Promise((res) => {
+      let posts = getData(token)
+      posts[id].deleted = true
+      res(posts[id])
+    })
+}
+
+function edit (token, id, post) {
+    return new Promise((res) => {
+        let posts = getData(token)
+        for (prop in post) {
+            posts[id][prop] = post[prop]
+        }
+        res(posts[id])
+    })
+}
+
+function incrementCommentCounter(token, id, count) {
+  const data = getData(token)
+  if (data[id]) {
+    data[id].commentCount += count
+  }
+}
+
+module.exports = {
+  get,
+  getAll,
+  getByCategory,
+  add,
+  vote,
+  disable,
+  edit,
+  getAll,
+  incrementCommentCounter
+}
