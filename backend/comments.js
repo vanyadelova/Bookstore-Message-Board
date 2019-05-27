18d9bc14be3505dd1a3bd11aa33bc9699f5312e5
vanyadelova <happy.vanya.delova@gmail.com>
2019-05-27 10:45:54 +0200

    help for server usage added

--- backend/comments.js
+++ backend/comments.js
@@ -8,7 +8,7 @@ const defaultData = {
     id: '894tuq4ut84ut8v4t8wun89g',
     parentId: "8xf0y6ziyjabvozdd253nd",
     timestamp: 1468166872634,
-    body: 'Books are amazing.',
+    body: 'Yes they are! :)',
     author: 'Marina',
     voteScore: 6,
     deleted: false,
